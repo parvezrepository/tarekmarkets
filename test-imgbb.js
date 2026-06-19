204b7b582de3b2afcf09dd08ed5c53797b68bad1
parvezrepository <parvezgithuboffial@gmail.com>
2026-06-19 21:00:49 +0600

    Remove test scripts

--- test-imgbb.js
+++
@@ -1,5 +0,0 @@
-const url = 'https://ibb.co/6PZbbjH'; // valid public imgbb viewer link
-fetch(url).then(res => res.text()).then(html => {
-  const match = html.match(/<meta property="og:image" content="([^"]+)"/);
-  console.log('Match:', match ? match[1] : 'No match');
-}).catch(console.error);
--- test-ua.js
+++
@@ -1,8 +0,0 @@
-const url = 'https://ibb.co/6PZbbjH';
-fetch(url, { headers: { 'User-Agent': 'Mozilla/5.0' } })
-  .then(res => res.text())
-  .then(html => {
-    const fs = require('fs');
-    fs.writeFileSync('imgbb.html', html);
-    console.log('Saved to imgbb.html');
-  }).catch(console.error);
